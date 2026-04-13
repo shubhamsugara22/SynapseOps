e8155410c10fbb4ffe66cf76dc3fc6d0d75485bb
shubhamsugara22 <shubhamsugara22@gmail.com>
2026-04-13 22:46:24 +0530

    ASGI issue fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,9 @@
-"""SynapseOps application package."""
+"""SynapseOps application package.
+
+This exposes an ASGI app object for platforms that auto-detect `app:app`.
+"""
+
+from app.api.server import create_app
+
+app = create_app()
+
